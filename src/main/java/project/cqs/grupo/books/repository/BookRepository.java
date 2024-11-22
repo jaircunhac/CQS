b9ca955a52e7db26c747c564a98fa97bec67e180
arrys <arryshanry@gmail.com>
2024-11-22 19:00:50 -0300

    File Fix

--- src/main/java/project/cqs/grupo/books/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package project.cqs.grupo.books.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.cqs.grupo.books.model.Book;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long>{
-}
